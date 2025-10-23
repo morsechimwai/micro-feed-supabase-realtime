117632595a941a35007730164a65f0685ad9d415
morsechimwai <morse.chimwai@gmail.com>
2025-10-23 15:30:58 +0700

    feat: add @vercel/analytics for improved analytics tracking

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Toaster } from "@/components/ui/sonner";
+import { Analytics } from "@vercel/analytics/next";
 import "./index.css";
 import App from "./App.tsx";
 
@@ -8,5 +9,6 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Toaster />
     <App />
+    <Analytics />
   </StrictMode>
 );
