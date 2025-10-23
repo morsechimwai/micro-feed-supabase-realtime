6246342e03c8b31afe718faf628bdf5d533cf331
morsechimwai <morse.chimwai@gmail.com>
2025-10-23 15:45:56 +0700

    fix: update Analytics import to use the correct package path

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Toaster } from "@/components/ui/sonner";
-import { Analytics } from "@vercel/analytics/next";
+import { Analytics } from "@vercel/analytics/react";
 import "./index.css";
 import App from "./App.tsx";
 
